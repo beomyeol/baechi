ef3789c54206294e63ed5740d4b861e4decb6ca0
Beomyeol Jeon <bj2@illinois.edu>
2020-12-06 11:12:15 -0600

    Minor fix on cifarnet.

--- image_classifier/networks/cifarnet.py
+++ image_classifier/networks/cifarnet.py
@@ -68,15 +68,15 @@ def cifarnet(images, num_classes=10, is_training=False,
       end_points['conv1'] = net
       net = slim.max_pool2d(net, [2, 2], 2, scope='pool1')
       end_points['pool1'] = net
-      net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm1', scope='norm1')
+      net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm1')
       net = slim.conv2d(net, 64, [5, 5], scope='conv2')
       end_points['conv2'] = net
-      net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm2', scope='norm2')
+      net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm2')
       net = slim.max_pool2d(net, [2, 2], 2, scope='pool2')
       end_points['pool2'] = net
       net = slim.flatten(net, scope='Flatten')
       end_points['Flatten'] = net
-      net = layslim.fully_connected(net, 384, scope='fc3')
+      net = slim.fully_connected(net, 384, scope='fc3')
       end_points['fc3'] = net
       net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
                            scope='dropout3')
